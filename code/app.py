7fdddabb5706135fbdaeff437e06b4d59792a94c
Jeff <theta_158@yahoo.com>
2019-09-14 22:23:34 -0400

    Added run.py so db imports run

--- code/app.py
+++ code/app.py
@@ -15,11 +15,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
 
-# Create database
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 # Create authentication object
 jwt = JWT(app, authenticate, identity)
 
